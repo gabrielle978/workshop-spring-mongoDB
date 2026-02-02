0b78645e09d7d00ebb99afcf7d7671e7b7a104b4
gabrielle978 <gbassiquete@outlook.com>
2026-02-02 20:53:29 -0300

    author's data projection using DTO

--- src/main/java/com/gbs/workshopmongo/config/instantiation.java
+++ src/main/java/com/gbs/workshopmongo/config/instantiation.java
@@ -1,6 +1,7 @@
 package com.gbs.workshopmongo.config;
 
 import com.gbs.workshopmongo.domain.Post;
+import com.gbs.workshopmongo.dto.authorDTO;
 import com.gbs.workshopmongo.repository.userRepository;
 import com.gbs.workshopmongo.domain.User;
 import com.gbs.workshopmongo.repository.postRepository;
@@ -38,10 +39,13 @@ public class instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex", "alexex@gmail.com");
         User alice = new User(null, "Alice", "alice@gmail.com");
 
-        Post post1= new Post(null, sdf.parse("21/03/2023"), "Bora viajar", "Me aguarde Bahia!", maria );
-        Post post2= new Post(null, sdf.parse("02/02/2026"), "Bom dia Brasil", "Ansiosa para a prova de hoje!", gabi );
-
+        //salvar primeiro os usuários p/ criar seus próprios id's
         userRepositoryConfig.saveAll(Arrays.asList(gabi, maria, alex, alice));
+
+        Post post1= new Post(null, sdf.parse("21/03/2023"), "Bora viajar", "Me aguarde Bahia!", new authorDTO(maria));
+        Post post2= new Post(null, sdf.parse("02/02/2026"), "Bom dia Brasil", "Ansiosa para a prova de hoje!", new authorDTO(gabi));
+
+        //depois salva-se o post com o id do usuário
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
--- src/main/java/com/gbs/workshopmongo/domain/Post.java
+++ src/main/java/com/gbs/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.gbs.workshopmongo.domain;
 
+import com.gbs.workshopmongo.dto.authorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -14,12 +15,12 @@ public class Post {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private authorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, authorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -59,11 +60,11 @@ public class Post {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public authorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(authorDTO author) {
         this.author = author;
     }
 
